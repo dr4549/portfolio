c62725788e09d9925e53fc37714983d28c2ce2ae
junghwan Kim <dr4549@naver.com>
2023-04-15 20:11:55 +0900

    파이어베이스

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,33 +1,71 @@
 <template>
   <div class="projects">
     <p class="title">Projects</p>
-    <div>
+    <div class="box">
       <p>To Do List</p>
-      <img src="@/assets/projects/todolist/todo1.png" />
-      ㅈㄷㄱㅈㄷㄱㅈㄷㄱㅈㄷㄱ
+      <swiper
+        :pagination="{
+          dynamicBullets: true,
+        }"
+        :modules="[Navigation, Pagination]"
+        :spaceBetween="20"
+        :loop="true"
+        class="mySwiper"
+      >
+        <swiper-slide>
+          <img src="@/assets/projects/todolist/todo1.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/projects/todolist/todo2.png" />
+        </swiper-slide>
+        <swiper-slide>
+          <img src="@/assets/projects/todolist/todo3.png" />
+        </swiper-slide>
+        <swiper-slide
+          ><img src="@/assets/projects/todolist/todo4.png" />
+        </swiper-slide>
+      </swiper>
+      asdf
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { Pagination, Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/vue";
+import "swiper/css";
+import "swiper/css/pagination";
+</script>
 <style lang="scss" scoped>
 .projects {
   position: relative;
   width: 100%;
-  display: flex;
-  align-items: center;
   flex-direction: column;
-  height: 600px;
   background-color: gray;
-
-  div {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  .box {
+    width: 70%;
     background-color: rgb(150, 211, 247);
-    padding: 50px;
+    padding: 40px;
     border-radius: 1rem;
     font-size: 2em;
     font-weight: 600;
-    img {
-      height: 100px;
-    }
+    text-align: center;
+  }
+  .swiper {
+    width: 100%;
+  }
+  .swiper-slide {
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+  }
+
+  .swiper-slide img {
+    width: 100%;
+    object-fit: cover;
+    cursor: pointer;
   }
 }
 </style>
